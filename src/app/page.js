f1cca1bdf7209d55bf8b97bd0d0ac5e43610543c
Kayode Dan Precious <danielkayode222@gmail.com>
2024-08-31 21:00:31 +0100

    refinig ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 "use client";
 
+import Image from "next/image";
 import { useState, useRef } from "react";
+import { BsSoundwave } from "react-icons/bs";
+import { FaStop } from "react-icons/fa6";
+import { FaMicrophoneAlt } from "react-icons/fa";
 
 const HomePage = () => {
   const [isRecording, setIsRecording] = useState(false);
@@ -39,21 +43,57 @@ const HomePage = () => {
   };
 
   return (
-    <div className="px-5">
-      <h1 className="">Audio Recorder</h1>
-      <button
-        className="p-2 bg-black text-white rounded-md"
-        onClick={isRecording ? stopRecording : startRecording}
-      >
-        {isRecording ? "Stop Recording" : "Start Recording"}
-      </button>
-      {audioUrl && (
-        <div className="">
-          <h3 className="">Recorded Audio</h3>
-          <audio controls src={audioUrl} />
+    <section className="px-5 h-[100vh]">
+      <div className="p-2 flex justify-center py-8">
+        <h1 className="px-6 py-3 bg-black text-white rounded-[3rem] font-bold flex items-center">
+          <span>
+            <BsSoundwave />
+          </span>
+          WebPodcaast
+          <span>
+            <BsSoundwave />
+          </span>
+        </h1>
+      </div>
+
+      <div className="h-[70%] flex justify-center flex-col border items-center w-full ">
+        <div className="md:w-[50%] w-full border">
+          <Image
+            src="/soundwave.gif"
+            width={100}
+            height={100}
+            alt="soundwave"
+            className="w-full object-cover"
+          />
         </div>
-      )}
-    </div>
+
+        <div className="my-10">
+          {audioUrl && (
+            <div className="text-center">
+              <h3 className="py-2">Your Audio</h3>
+              <audio controls src={audioUrl} />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="flex justify-center text-[2rem]">
+        <button
+          className="p-2 bg-black text-white rounded-md"
+          onClick={isRecording ? stopRecording : startRecording}
+        >
+          {isRecording ? (
+            <span className="">
+              <FaStop />
+            </span>
+          ) : (
+            <span>
+              <FaMicrophoneAlt />
+            </span>
+          )}
+        </button>
+      </div>
+    </section>
   );
 };
 
